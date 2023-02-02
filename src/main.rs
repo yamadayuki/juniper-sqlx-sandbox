abda69b2ae9509f2ba81b06fdf45b1d1980c605c
Yamada Yuki <yamadayuuki0818@gmail.com>
2023-02-02 19:34:09 +0900

    Implement healthz endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpServer, Responder};
+
+#[get("/healthz")]
+async fn healthz() -> impl Responder {
+    "OK".to_string()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(healthz))
+        .bind(("127.0.0.1", 8082))?
+        .run()
+        .await
 }
