76882cda2f995e577c200eec7990d7c7568e6f6b
Yamada Yuki <yamadayuuki0818@gmail.com>
2023-02-02 19:37:39 +0900

    Use tokio

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ async fn healthz() -> impl Responder {
     "OK".to_string()
 }
 
-#[actix_web::main]
+#[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(healthz))
         .bind(("127.0.0.1", 8082))?
